70cb6acef128aa8e9fdd6fd26613b1c76f9421f9
Christine Anani <ananichristine2333@gmail.com>
2020-06-23 11:26:57 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import HomePage from "./pages/Homepage/homepage.component";
+
+export default function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
